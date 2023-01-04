8f5aa18629bf276ed3d30b5f54e8d42e66d5891b
Fatema04 <shadrinmoni15@gmail.com>
2023-01-04 18:43:28 +0600

    readme edited

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from './components/Buttons';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Buttons = () => {
+    return (
+        <div>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </div>
+    );
+};
+
+export default Buttons;
